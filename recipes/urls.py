83162f80a6c532af483c70a1bd6ccf82967f0951
AurélioMSFilho <aureliofilhotj@gmail.com>
2024-01-13 13:30:34 -0300

    paginas web parte 1

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from recipes.views import contato, home, sobre
+from recipes.views import contato, home, sobre, biblioteca, audio_video
 
 urlpatterns = [
     path('', home ),
     path('sobre/', sobre ),
     path('contato/', contato),
+    path('biblioteca/', biblioteca),
+    path('audio_video/', audio_video )
 ]
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse ('HOME 1')
+    return render (request, 'home.html')
 
 def sobre(request):
     return HttpResponse ('sobre')
 
 def contato(request):
     return HttpResponse ('contato')
+
+def biblioteca(request):
+    return HttpResponse('biblioteca')
+
+def audio_video(request):
+    return render(request, 'audio.html')
+
