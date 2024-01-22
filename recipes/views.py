4b465aa773d0fabaa069505ee43ea2bcef04fd29
AurélioMSFilho <aureliofilhotj@gmail.com>
2024-01-22 19:43:14 -0300

    templates

--- recipes/views.py
+++ recipes/views.py
@@ -5,13 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render (request, 'home.html')
+    return render (request, 'recipes/home.html', context={
+        'nome': 'Aurélio Filho', 
+    })
 
 def sobre(request):
     return render (request,'arquivo.html')
 
 def contato(request):
-    return HttpResponse ('contato')
+    return render (request, 'temp.html')
 
 def biblioteca(request):
     return HttpResponse('biblioteca')
